b69f2f859c9241c215b947ead957d9db326f9f0b
arjun-mehndiratta <arjun.mehndiratta@gmail.com>
2022-06-05 20:41:00 +0530

    Update streamlit_app.py
    added default selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #pick list
-streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index),['Avocado','Strwaberries'])
 #display the  result
 streamlit.dataframe(my_fruit_list)
 
